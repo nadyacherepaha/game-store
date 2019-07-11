27918f6341efc3e6e929786fdfc1e410daf24c1f
Yegor G <yegorich_555@mail.ru>
2019-07-11 14:58:24 +0300

    FriendlyErrorsWebpackPlugin. Rollback to default (since custom functions has bugs)

--- webpack.common.js
+++ webpack.common.js
@@ -39,48 +39,10 @@ module.exports = {
             }
         }),
         new CaseSensitivePathsPlugin(),
-        new FriendlyErrorsWebpackPlugin({
+        new FriendlyErrorsWebpackPlugin(),
             additionalTransformers: [
                 error => {
                     if (error.webpackError) {
-                        const message = typeof error.webpackError === 'string' ?
-                            error.webpackError :
-                            error.webpackError.message || ''
-                        for (const {
-                                re,
-                                msg,
-                                type
-                            } of rules) {
-                            const match = message.match(re)
-                            if (match) {
-                                return Object.assign({}, error, {
-                                    // type is necessary to avoid being printed as defualt error
-                                    // by friendly-error-webpack-plugin
-                                    type,
-                                    shortMessage: msg(error, match)
-                                })
-                            }
-                        }
-                        // no match, unknown webpack error without a message.
-                        // friendly-error-webpack-plugin fails to handle this.
-                        if (!error.message) {
-                            return Object.assign({}, error, {
-                                type: 'unknown-webpack-error',
-                                shortMessage: message
-                            })
-                        }
-                    }
-                    return error
-                }
-            ],
-            additionalFormatters: [
-                errors => {
-                    errors = errors.filter(e => e.shortMessage)
-                    if (errors.length) {
-                        return errors.map(e => e.shortMessage)
-                    }
-                }
-            ]
         }),
         new HtmlWebpackPlugin({
             template: path.resolve(publicPath, "index.html"),
