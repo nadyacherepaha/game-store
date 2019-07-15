1a4a8669e41c57a09e48fc71e3b39cb071d3f983
Yegor G <yegorich_555@mail.ru>
2019-07-15 15:08:19 +0300

    Styles. Add postcss-normalize

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
-module.exports = {
+module.exports = { //browser target config see in .browserlistsrc
     plugins: {
-        autoprefixer: {}
+        autoprefixer: {}, //it adds vendore prefixes ::placeholder => ::-webkit-input-placeholder, ::-moz-placeholder etc. https://github.com/postcss/autoprefixer
+        "postcss-normalize": { //it adds normalize.css and sanitize.css
+            forceImport: true,
+            allowDuplicates: false
+        }
     }
 };
