007c7b774f8c85c9e9437a7163d8259bd3a8b79c
Yegor G <yegorich_555@mail.ru>
2019-11-27 11:15:40 +0300

    Babel. Add plugin jsx-classnames-advanced and config proposal-class-properties

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"]
+  presets: [
+    "@babel/preset-env", // allows you to use the latest JavaScript
+    "@babel/preset-react" // optional: react: this resolves react-files (jsx, tsx)
+  ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties", // transforms static class properties as well as properties declared with the property initializer syntax
+    "jsx-classnames-advanced" // optional: react: this resolves className={object}
+  ]
 };
