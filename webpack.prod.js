eb86512bedcd3a1c2b59fedd6480f07bba7921da
Yegor G <yegorich_555@mail.ru>
2019-12-12 16:14:29 +0300

    Webpack. Improve config. Add comments

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,32 +10,33 @@ module.exports = (env, argv) => {
 
   const result = merge(commonConfig, {
     mode: "production",
-    devtool: common.enableSourceMap ? "source-map" : "none",
+    devtool: common.enableSourceMap ? "source-map" : "none", // option controls how source maps are generated (affects on build speed dramatically): https://v4.webpack.js.org/configuration/devtool/
     output: {
-      filename: "[name].[contenthash:8].js",
+      filename: "[name].[contenthash:8].js", // contenthash-this is version for avoing browser-cache issue: user always had to get the last version of files
       chunkFilename: "[name].[contenthash:8].js"
     },
     performance: {
       assetFilter: function assetFilter(assetFilename) {
-        return !/(\.map$)|(fonts)|(images)/.test(assetFilename);
+        return !/(\.map$)|(fonts)|(images)/.test(assetFilename); // ignore these files from perfomance-hints
       }
     },
     optimization: {
       minimizer: [
         new TerserPlugin({
+          // default webpack plugin for js-optimization which should be configured: https://v4.webpack.js.org/configuration/optimization/#optimizationminimizer
           // speedest alternative of UglifyJS (it improves minifying js files)
           test: /\.m?js(\?.*)?$/i,
-          chunkFilter: () => true,
-          warningsFilter: () => true,
-          extractComments: false, // disable extracting comments to a different file (since we don't have this)
-          sourceMap: common.enableSourceMap,
+          chunkFilter: () => true, // set false if we don't need uglifying (for debug purpose)
+          warningsFilter: () => true, // removing console.warn from files
+          extractComments: false, // disable extracting comments to a different file
+          sourceMap: common.enableSourceMap, // generating *.map.js files
           cache: (argv && argv.hot) || false, // enable caching only for hot-replacement-mode
           terserOptions: {
             output: {
               comments: false // remove comments from files
             },
             mangle: {
-              safari10: true // for preventing safari10 bugs
+              safari10: true // for preventing Safari 10/11 bugs in loop scoping and await
             }
           }
         }),
@@ -45,12 +46,11 @@ module.exports = (env, argv) => {
     plugins: [
       // additional config for plugins is placed in webpack.common.js
       new CompressionPlugin({
-        // it creates gzipped (compressed) files in '[path].gz[query]'
+        // optional: it creates gzipped (compressed) files in '[path].gz[query]'
         threshold: common.filesThreshold // (bytes). Only assets bigger than this size are processed
       })
     ]
   });
 
-  // console.warn("merge-prod-result:\r\n", result);
   return result;
 };
