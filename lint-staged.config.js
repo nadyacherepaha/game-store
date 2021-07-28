d262d75d6244c896e92246a78f0a9f61bd1e57b9
Yahor H <yegorich_555@mail.ru>
2021-07-28 15:42:56 +0300

    Linters. Add gitHook, husky & lintStaged

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // run tsc on changes to TypeScript files
+  "*.ts?(x)": () => "tsc -p tsconfig.app.json --noEmit",
+  "*.{css,scss,less}": ["stylelint --fix --cache", "prettier --write"],
+  "*.{js,ts,json}": ["eslint --fix --cache", "prettier --write"],
+  // optional: disable prettier for html files (via removing next line)
+  "*.{html}": ["prettier --write"],
+  "*.{md}": ["markdownlint --fix **/*.md"],
+};
