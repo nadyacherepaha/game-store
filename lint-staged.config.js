a4335a473fc3e1ff6413674110d3b090ddad33c7
Yahor H <yegorich_555@mail.ru>
2021-09-07 14:18:20 +0300

    Husky. Fix lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // run tsc on changes to TypeScript files
-  "*.ts?(x)": () => "tsc -p tsconfig.app.json --noEmit",
+  "*.ts?(x)": () => "tsc -p tsconfig.json --noEmit",
   "*.{css,scss,less}": ["stylelint --fix --cache", "prettier --write"],
   "*.{js,ts,json}": ["eslint --fix --cache", "prettier --write"],
   // optional: disable prettier for html files (via removing next line)
