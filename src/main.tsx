26f32734e4ed2752006a750f24ecd81c1e5611ec
icx\nadzeya.charapakha <n.cherepaha@gmail.com>
2021-10-18 11:51:43 +0300

    Fixed Readme.md and deleted unnecessary files

--- src/main.tsx
+++ src/main.tsx
@@ -1,55 +1,26 @@
 import "./styles/main.css";
 import "./styles/main.scss";
 // watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
-import imgSmall from "images/testSmall.png"; // start-path is 'images' because we have an alias 'images' in webpack.common.js
-import imgCamera from "images/camera.svg";
 import { Component, StrictMode } from "react";
 import ReactDom from "react-dom";
 import style from "./styles/main.module.css";
-import someTypeScript from "./someTypeScript";
 
-interface AppProps {
-  nothing: boolean;
-}
 interface AppState {
   title: string;
 }
 
-class AppContainer extends Component<AppProps, AppState> {
+class AppContainer extends Component<AppState> {
   ["constructor"]: typeof AppContainer;
-
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      title: someTypeScript("Test-block for css-modules"),
-    };
-    // test class-dead-code
-    const goExlcude = true;
-    if (!goExlcude) {
-      console.warn("class-dead-code doesn't work");
-    }
-  }
 
   render() {
     return (
       <StrictMode>
         <div className="test-block">
-          <h2 className={style.mainTitle}>{this.state.title}</h2>
-        </div>
-        <div className={["test-block", style.background].join(" ")}>
-          <h2>Test-block for url-loader</h2>
-          <img src={imgSmall} alt="smallImage" />
-        </div>
-        {/*  or it can be
-          <img src='/src/images/testSmall.png' alt="smallImage"></img>
-        */}
-        <div className={["test-block", style.svgBackground].join(" ")}>
-          <h2>Test-block for svg-url-loader</h2>
-          <img src={imgCamera} alt="small_SVG_Image" />
+          <h2 className={style.mainTitle}>Hi</h2>
         </div>
       </StrictMode>
     );
   }
 }
 
-ReactDom.render(<AppContainer nothing={false} />, document.getElementById("app"));
+ReactDom.render(<AppContainer title={`''`} />, document.getElementById("app"));
