abcd468eecc87c52870b19f62bb5b5bc59f0256d
nadyacherepaha <nad.cherepaha@gmail.com>
2021-10-25 12:43:19 +0300

    Move footer items to the constant

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,34 +1,18 @@
 import React, { FC } from "react";
-import SonyLogo from "../../assets/images/sonyLogo.svg";
-import UbisoftLogo from "../../assets/images/ubisoftLogo.svg";
-import ValveLogo from "../../assets/images/valveLogo.svg";
-import NintendoLogo from "../../assets/images/nintendoLogo.svg";
 import style from "./footer.module.scss";
+import footerItem from "../../constants/footerItem";
 
 const Footer: FC = () => (
   <footer className={style.footer}>
     <span>Incredible convenient</span>
     <div className={style.list}>
-      <div className={style.item}>
-        <a href="https://www.sony.ru/">
-          <img src={SonyLogo} alt="" />
-        </a>
-      </div>
-      <div className={style.item}>
-        <a href="https://www.nintendo.ru/">
-          <img src={NintendoLogo} alt="" />
-        </a>
-      </div>
-      <div className={style.item}>
-        <a href="https://www.ubisoft.com/ru-ru/">
-          <img src={UbisoftLogo} alt="" />
-        </a>
-      </div>
-      <div className={style.item}>
-        <a href="https://www.valvesoftware.com/ru/">
-          <img src={ValveLogo} alt="" />
-        </a>
-      </div>
+      {footerItem.map(({ linkRef, imageLink, alt }) => (
+        <div className={style.item}>
+          <a href={linkRef}>
+            <img src={imageLink} alt={alt} />
+          </a>
+        </div>
+      ))}
     </div>
     <span> © LoremIpsum, 2021-2022</span>
   </footer>
