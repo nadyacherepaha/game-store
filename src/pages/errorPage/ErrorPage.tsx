75469d2e17e11978f19637fa3b2babe56c2cafa6
nadyacherepaha <nad.cherepaha@gmail.com>
2021-10-25 13:16:25 +0300

    Add a page reload button

--- src/pages/errorPage/ErrorPage.tsx
+++ src/pages/errorPage/ErrorPage.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from "react";
-import { AlertTitle, Alert } from "@mui/material";
+import { AlertTitle, Alert, Button } from "@mui/material";
 import style from "./errorPage.module.scss";
+
+const reloadPage = () => window.location.reload();
 
 const ErrorPage: FC = () => (
   <div className={style.errorMessage}>
@@ -9,6 +11,10 @@ const ErrorPage: FC = () => (
         <strong>Oh-no! Something went wrong</strong>
       </AlertTitle>
       You can refresh the page or try again later.
+      <br />
+      <Button className={style.btn} variant="outlined" color="error" value="Reload Page" onClick={reloadPage}>
+        Reload the page
+      </Button>
     </Alert>
   </div>
 );
