3ed04c0d9b98621bcdf5b251100c600058142e99
nadyacherepaha <nad.cherepaha@gmail.com>
2021-10-27 08:38:06 +0300

    Move the callback function from ErrorBoundary to ErrorPage

--- src/pages/error-page/ErrorPage.tsx
+++ src/pages/error-page/ErrorPage.tsx
@@ -2,21 +2,22 @@ import React, { FC } from "react";
 import { AlertTitle, Alert, Button } from "@mui/material";
 import style from "./errorPage.module.scss";
 
-const onReloadPage = () => window.location.reload();
-
-const ErrorPage: FC = () => (
-  <div className={style.errorMessage}>
-    <Alert className={style.title} severity="error">
-      <AlertTitle>
-        <strong>Oh-no! Something went wrong</strong>
-      </AlertTitle>
-      You can refresh the page or try again later.
-      <br />
-      <Button className={style.btn} variant="outlined" color="error" value="Reload Page" onClick={onReloadPage}>
-        Reload the page
-      </Button>
-    </Alert>
-  </div>
-);
+const ErrorPage: FC = () => {
+  const onReloadPage = () => window.location.reload();
+  return (
+    <div className={style.errorMessage}>
+      <Alert className={style.title} severity="error">
+        <AlertTitle>
+          <strong>Oh-no! Something went wrong</strong>
+        </AlertTitle>
+        You can refresh the page or try again later.
+        <br />
+        <Button className={style.btn} variant="outlined" color="error" value="Reload Page" onClick={onReloadPage}>
+          Reload the page
+        </Button>
+      </Alert>
+    </div>
+  );
+};
 
 export default ErrorPage;
