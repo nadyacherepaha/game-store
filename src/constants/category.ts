1a03f9c206bf6890ae1d23e8c23459ffd56fdc51
nadyacherepaha <nad.cherepaha@gmail.com>
2021-10-28 18:31:44 +0300

    Create a Categories component

---
+++ src/constants/category.ts
@@ -0,0 +1,21 @@
+import { faXbox, faWindows, faPlaystation } from "@fortawesome/free-brands-svg-icons";
+
+const category = [
+  {
+    icon: faWindows,
+    title: "PC",
+    link: "/home",
+  },
+  {
+    icon: faPlaystation,
+    title: "Playstation 5",
+    link: "/home",
+  },
+  {
+    icon: faXbox,
+    title: "XBox One",
+    link: "/home",
+  },
+];
+
+export default category;
