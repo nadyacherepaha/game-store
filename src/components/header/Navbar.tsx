220f1792c08c5cb76705be1274cfdbcab637b19d
nadyacherepaha <nad.cherepaha@gmail.com>
2021-10-28 17:08:35 +0300

    Add a drop-down navigation menu

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { NavLink, Link } from "react-router-dom";
-import { Dropdown } from "react-bootstrap";
+import { Dropdown, NavItem } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import style from "./navbar.module.scss";
 
@@ -11,24 +11,22 @@ const Navbar: FC = () => (
         Home
       </NavLink>
 
-      <NavLink to="#">
-        <Dropdown className={style.dropdown}>
-          <Dropdown.Toggle className={style.btn} variant="dark" id="dropdown-basic">
-            Products
-          </Dropdown.Toggle>
-          <Dropdown.Menu className={style.dropdownMenu}>
-            <Dropdown.Item className={style.dropdownItem} eventKey="baudratestate2400">
-              <Link to="/products">PC</Link>
-            </Dropdown.Item>
-            <Dropdown.Item className={style.dropdownItem} eventKey="baudratestate4800">
-              <Link to="/home">Playstation 5</Link>
-            </Dropdown.Item>
-            <Dropdown.Item className={style.dropdownItem} eventKey="baudratestate2400">
-              <Link to="/about">XBox One</Link>
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </NavLink>
+      <Dropdown className={style.dropdown}>
+        <Dropdown.Toggle className={style.btn} variant="dark" id="dropdown-basic">
+          Products
+        </Dropdown.Toggle>
+        <Dropdown.Menu className={style.dropdownMenu}>
+          <Dropdown.Item as={NavItem} className={style.dropdownItem} eventKey="baudratestate2400">
+            <Link to="/products">PC</Link>
+          </Dropdown.Item>
+          <Dropdown.Item as={NavItem} className={style.dropdownItem} eventKey="baudratestate4800">
+            <Link to="/home">Playstation 5</Link>
+          </Dropdown.Item>
+          <Dropdown.Item as={NavItem} className={style.dropdownItem} eventKey="baudratestate2400">
+            <Link to="/about">XBox One</Link>
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
 
       <NavLink className={style.item} to="/about">
         About
