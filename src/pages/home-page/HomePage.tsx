bc6ffe3c667793dd3ba467919fe416856382851a
nadyacherepaha <nad.cherepaha@gmail.com>
2021-11-02 09:41:41 +0300

    Update games cards

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -3,6 +3,7 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import style from "../../styles/main.module.css";
 import Categories from "../../components/categories/Categories";
+import SearchResult from "../../components/search/SearchResult";
 import Games from "../../components/games/Games";
 
 const HomePage: FC = () => (
@@ -10,8 +11,9 @@ const HomePage: FC = () => (
     <Header />
     <div className={style.wrapper}>
       <div className={style.content}>
+        <SearchResult />
         <Categories />
-        <Games />
+        <Games<ICardGameProps> />
       </div>
     </div>
     <Footer />
