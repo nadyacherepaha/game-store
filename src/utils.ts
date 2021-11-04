ebcbb9b72ebc79c2f6dc13af69d27cd99a56aa00
nadyacherepaha <nad.cherepaha@gmail.com>
2021-11-04 22:41:32 +0300

    Change utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,1 @@
-export const BASE_URL = "http://localhost:8079/games?";
-export const fetchSearchResults = async (query) => {
-  if (query && query.length > 0) {
-    const parsedQuery = query.replaceAll(" ", "+");
-    const url = `${BASE_URL}/games?search=${parsedQuery}`;
-    const res = await fetch(url);
-    const resJson = res.json();
-    return resJson;
-  }
-  return [];
-};
+export const BASE_URL = "http://localhost:8079";
