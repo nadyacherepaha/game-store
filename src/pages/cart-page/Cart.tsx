58c2effe733d2c73057d901a505992bc7c131f48
nadyacherepaha <nad.cherepaha@gmail.com>
2021-11-14 13:04:23 +0300

    Add cart and profile pages

---
+++ src/pages/cart-page/Cart.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+const Cart: FC = () => (
+  <>
+    <h1>Cart</h1>
+  </>
+);
+
+export default Cart;
---
+++ src/pages/profile-page/Profile.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+const Profile: FC = () => (
+  <>
+    <h1>Profile</h1>
+  </>
+);
+
+export default Profile;
