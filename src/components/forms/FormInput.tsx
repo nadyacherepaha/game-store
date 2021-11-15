0e00f667568df260c313e600c91d50c6753db8e4
nadyacherepaha <nad.cherepaha@gmail.com>
2021-11-15 09:46:18 +0300

    Create a reusable input field for forms

--- src/components/forms/FormInput.tsx
+++ src/components/forms/FormInput.tsx
@@ -3,11 +3,11 @@ import { FieldRenderProps } from "react-final-form";
 
 type Props = FieldRenderProps<string, any>;
 
-const LoginInput: React.FC<Props> = ({ input, meta, ...rest }: Props) => (
+const FormInput: React.FC<Props> = ({ input, meta, ...rest }: Props) => (
   <>
-    <input type="text" {...input} {...rest} />
+    <input {...input} {...rest} />
     {meta.error && meta.touched && <span>{meta.error}</span>}
   </>
 );
 
-export default LoginInput;
+export default FormInput;
