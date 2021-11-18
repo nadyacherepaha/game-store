aa91d8deb0aada1431017cc52a15deaf99679554
nadyacherepaha <nad.cherepaha@gmail.com>
2021-11-18 17:27:00 +0300

    Update the interface for user state

--- src/types/storyTypes.ts
+++ src/types/storyTypes.ts
@@ -1,18 +1,5 @@
-export enum UserActionTypes {
-  USER_LOGIN = "USER_LOGIN",
-  USER_LOGOUT = "USER_LOGOUT",
-}
-
 export interface IUserState {
-  user: null | boolean;
+  user: boolean;
+  signIn?: (userName: string) => void;
+  signOut?: () => void;
 }
-
-interface IUserLoginAction {
-  type: UserActionTypes.USER_LOGIN;
-}
-
-interface IUserLogoutAction {
-  type: UserActionTypes.USER_LOGOUT;
-}
-
-export type UserAction = IUserLoginAction | IUserLogoutAction;
