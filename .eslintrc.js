c999c2f66c6b6c9f62a200b4ba178ac256677857
nadyacherepaha <nad.cherepaha@gmail.com>
2021-11-18 17:30:50 +0300

    Update userReducer

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,6 +45,7 @@ module.exports = {
     "jsx-a11y/label-has-associated-control": 0,
     "consistent-return": 0,
     curly: "error",
+    "no-param-reassign": 0,
     // "react/jsx-max-props-per-line": [1, { maximum: 1 }], //it doesn't work with prettier, you can remove prettier from rules: 'prettier/prettier'...
     // "react/jsx-first-prop-new-line": [1, "multiline"], //it doesn't work with prettier, you can remove prettier from rules: 'prettier/prettier'...
     "react/prop-types": 0,
--- src/redux/reducers/userReducer.ts
+++ src/redux/reducers/userReducer.ts
@@ -1,18 +1,32 @@
-import { UserAction, IUserState, UserActionTypes } from "../../types/storyTypes";
+import { createSlice } from "@reduxjs/toolkit";
+import { IUserState } from "../../types/storyTypes";
+import { getCurrentUser } from "../../services/auth.service";
 
-const initialState: IUserState = {
-  user: null,
-};
-
-const userReducer = (state = initialState, action: UserAction): IUserState => {
-  switch (action.type) {
-    case UserActionTypes.USER_LOGIN:
-      return { user: true };
-    case UserActionTypes.USER_LOGOUT:
-      return { user: false };
-    default:
-      return state;
+const currentUser = () => {
+  if (getCurrentUser()) {
+    return true;
   }
+
+  return false;
 };
 
-export default userReducer;
+export const initialState: IUserState = {
+  user: currentUser(),
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signIn(state, action) {
+      state.user = true;
+      state.signIn = action.payload;
+    },
+    signOut(state, action) {
+      state.user = false;
+      state.signOut = action.payload;
+    },
+  },
+});
+
+export default userSlice.reducer;
