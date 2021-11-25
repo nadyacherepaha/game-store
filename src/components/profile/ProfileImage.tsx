71c7d8bc192fad37c7540c8e0b929f209cb4b425
nadyacherepaha <nad.cherepaha@gmail.com>
2021-11-25 18:13:36 +0300

    Move the default avatar url to a constant

--- src/components/profile/ProfileImage.tsx
+++ src/components/profile/ProfileImage.tsx
@@ -7,7 +7,8 @@ import { IUserProfile } from "../../types/UserProfile";
 interface IProfileImage extends IUserProfile {}
 
 const ProfileImage: FC<IProfileImage> = ({ avatar }) => {
-  const isImg = avatar || "https://winnote.ru/wp-content/uploads/2016/01/1454222417_del_recent_avatar1.png";
+  const defaultAvatarUrl = "https://winnote.ru/wp-content/uploads/2016/01/1454222417_del_recent_avatar1.png";
+  const isImg = avatar || defaultAvatarUrl;
   const [image, setImage] = useState(isImg);
 
   const handleFiles = (files) => {
