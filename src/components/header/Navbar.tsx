84b1d8b369b53339de64ab0405831724d7601f28
nadyacherepaha <nad.cherepaha@gmail.com>
2021-11-25 19:04:46 +0300

    Change the import to get the user

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -12,7 +12,7 @@ import { useAppSelector, useAppDispatch } from "../../hooks/redux";
 import { deleteUserFromLocalStorage } from "../../redux/actions/userActions";
 import getUser from "../../redux/selectors/authSelectors";
 import { userSlice } from "../../redux/reducers/userReducer";
-import { getCurrentUser } from "../../services/auth.service";
+import { getCurrentUserExists } from "../../services/auth.service";
 
 const Navbar: FC = () => {
   const { user } = useAppSelector(getUser);
@@ -23,7 +23,7 @@ const Navbar: FC = () => {
   const handleClick = (e: string): void => history.push(e);
 
   React.useEffect(() => {
-    if (getCurrentUser()) {
+    if (getCurrentUserExists()) {
       dispatch(signInUserInLocalStorage());
     }
   }, []);
