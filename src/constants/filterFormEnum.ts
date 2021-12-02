9e6b5e02f80702af397485e6eaa475f96b904f68
nadyacherepaha <nad.cherepaha@gmail.com>
2021-12-02 16:44:36 +0300

    Move the enum to a separate file and remove the constants

---
+++ src/constants/filterFormEnum.ts
@@ -0,0 +1,28 @@
+export enum Genres {
+  All = "",
+  Shooter = "Shooter",
+  Sandbox = "Sandbox",
+  RPG = "RPG",
+  Action = "Action-adventure",
+  Simulator = "Simulator",
+}
+
+export enum Ages {
+  All = "",
+  Three = "3",
+  Six = "6",
+  Twelve = "12",
+  Sixteen = "16",
+  Eighteen = "18",
+}
+
+export enum Criteria {
+  Name = "name",
+  Rating = "rating",
+  Price = "price",
+}
+
+export enum Types {
+  Ascending = "ascending",
+  Descending = "descending",
+}
--- src/types/FilterForm.ts
+++ src/types/FilterForm.ts
@@ -1,31 +1,4 @@
-export const enum Genres {
-  All = "",
-  Shooter = "Shooter",
-  Sandbox = "Sandbox",
-  RPG = "RPG",
-  Action = "Action-adventure",
-  Simulator = "Simulator",
-}
-
-export enum Ages {
-  All = "",
-  Three = "3",
-  Six = "6",
-  Twelve = "12",
-  Sixteen = "16",
-  Eighteen = "18",
-}
-
-export const enum Criteria {
-  Name = "name",
-  Rating = "rating",
-  Price = "price",
-}
-
-export const enum Types {
-  Ascending = "ascending",
-  Descending = "descending",
-}
+import { Genres, Ages, Criteria, Types } from "../constants/filterFormEnum";
 
 export interface IFilterFormValues {
   criteria?: Criteria;
