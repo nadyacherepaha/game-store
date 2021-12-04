8f0e62f2bf6faf579446c077b00baaf35b0272b0
nadyacherepaha <nad.cherepaha@gmail.com>
2021-12-04 20:59:43 +0300

    Add reducer to calculate subtotals

--- src/redux/reducers/cartReducer.ts
+++ src/redux/reducers/cartReducer.ts
@@ -3,10 +3,14 @@ import { Card } from "../../types/Card";
 
 interface ICart {
   itemInCart: Card[];
+  cartTotalAmount: string;
+  cartTotalQuantity: number;
 }
 
 const initialState: ICart = {
   itemInCart: [],
+  cartTotalAmount: "0",
+  cartTotalQuantity: 0,
 };
 
 const sliceCart = "cart";
@@ -23,22 +27,35 @@ export const cartSlice = createSlice({
         state.itemInCart.push(action.payload);
       }
     },
-    removeFromCart: (state, action) => {
+    removeFromCart: (state: ICart, action) => {
       state.itemInCart = state.itemInCart.filter((item) => item.id !== action.payload);
     },
-    increaseQuantity: (state, action) => {
-      const cartItem = state.itemInCart.find((item) => item.id !== action.payload.id);
-      cartItem && cartItem.amount++;
-    },
-    decreaseQuantity: (state, action) => {
-      const cartItem = state.itemInCart.find((item) => item.id !== action.payload.id);
+    decreaseQuantity: (state: ICart, action: PayloadAction<Card>) => {
+      const cartItem = state.itemInCart.find((item) => item.id === action.payload.id);
       cartItem && cartItem.amount--;
     },
     clearCart: (state) => {
       state.itemInCart = [];
     },
+    getTotals: (state: ICart) => {
+      const { cost, quantity } = state.itemInCart.reduce(
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem;
+          const itemTotal = price * amount;
+
+          cartTotal.cost += itemTotal;
+          cartTotal.quantity += amount;
+
+          return cartTotal;
+        },
+        { cost: 0, quantity: 0 }
+      );
+
+      state.cartTotalQuantity = quantity;
+      state.cartTotalAmount = cost.toFixed(2);
+    },
   },
 });
 
-export const { addToCart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, decreaseQuantity, clearCart, getTotals } = cartSlice.actions;
 export default cartSlice.reducer;
