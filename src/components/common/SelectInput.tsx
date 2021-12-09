414895c0f74d8da6570eccf60f4a0ea23a74d882
nadyacherepaha <nad.cherepaha@gmail.com>
2021-12-09 15:05:46 +0300

    Pass parameters as an array

--- src/components/common/SelectInput.tsx
+++ src/components/common/SelectInput.tsx
@@ -5,10 +5,23 @@ interface ISelectInput extends FieldRenderProps<string> {
   title: string;
 }
 
+export interface ISelectedItem {
+  key: number;
+  value: string;
+}
+
+export const items: ISelectedItem[] = [
+  { key: 1, value: "1" },
+  { key: 2, value: "2" },
+  { key: 3, value: "3" },
+  { key: 4, value: "4" },
+  { key: 5, value: "5" },
+];
+
 const SelectInput: React.FC<ISelectInput> = ({ title, input, ...rest }: ISelectInput) => (
   <>
     <span>{title}</span>
-    <select {...input} {...rest} />
+    <select {...input} {...items} {...rest} />
   </>
 );
 
--- src/components/forms/edit-card-form/RatingField.tsx
+++ src/components/forms/edit-card-form/RatingField.tsx
@@ -1,16 +1,18 @@
 import React, { FC } from "react";
 import { Field } from "react-final-form";
-import SelectInput from "../../common/SelectInput";
+import SelectInput, { items } from "../../common/SelectInput";
 import style from "./editCardForm.module.scss";
 
 const RatingField: FC = () => (
   <div className={style.field}>
     <Field className={style.select} title="Rating" name="rating" component={SelectInput}>
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
+      {items.map((result) => (
+        <>
+          <option key={result.key} value={result.value}>
+            {result.value}
+          </option>
+        </>
+      ))}
     </Field>
   </div>
 );
