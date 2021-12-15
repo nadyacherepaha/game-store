6758a1120b6534d0d49701967629b4466a954b37
nadyacherepaha <nad.cherepaha@gmail.com>
2021-12-15 12:00:36 +0300

    Move strings to constants

--- src/tests/e2e.test.tsx
+++ src/tests/e2e.test.tsx
@@ -3,6 +3,8 @@ import puppeteer from "puppeteer";
 describe("HomePage.tsx", () => {
   let browser;
   let page;
+  const componentsCategories = "components-categories-categories";
+  const componentsGames = "components-games-card-game-cardGame";
 
   beforeAll(async () => {
     browser = await puppeteer.launch();
@@ -15,24 +17,27 @@ describe("HomePage.tsx", () => {
   });
 
   it("displayed categories", async () => {
-    await page.waitForSelector(".components-categories-categories__wrapper");
-    const title = await page.$eval(".components-categories-categories__title", (e) => e.innerHTML);
-    const button = await page.$eval(".components-categories-categories__items", (e) => !!e);
+    await page.waitForSelector(`.${componentsCategories}__wrapper`);
+    const title = await page.$eval(`.${componentsCategories}__title`, (e) => e.innerHTML);
+    const button = await page.$eval(`.${componentsCategories}__items`, (e) => !!e);
     const linkItems = await page.$$(".components-categories-category-item-categoryItem__item");
+    const linkItemsQuantity = 3;
+    const categoriesTitle = "Categories";
 
-    expect(title).toContain("Categories");
+    expect(title).toContain(categoriesTitle);
     expect(button).toBeTruthy();
-    expect(linkItems.length).toBe(3);
+    expect(linkItems.length).toBe(linkItemsQuantity);
   });
 
   it("displayed top games", async () => {
-    await page.waitForSelector(".components-categories-categories__wrapper");
-    const topGames = await page.$eval(".components-games-card-game-cardGame__cards", (e) => !!e);
-    const cardGame = await page.$$(".components-games-card-game-cardGame__card");
+    await page.waitForSelector(`.${componentsCategories}__wrapper`);
+    const topGames = await page.$eval(`.${componentsGames}__cards`, (e) => !!e);
+    const cardGame = await page.$$(`.${componentsGames}__card`);
     const buttonAddToCart = await page.$eval(".styles-main__btn", (e) => !!e);
+    const cardGameQuantity = 3;
 
     expect(topGames).toBeTruthy();
-    expect(cardGame.length).toBe(3);
+    expect(cardGame.length).toBe(cardGameQuantity);
     expect(buttonAddToCart).toBeTruthy();
   });
 
